6bb4f2b7117aca4d2c2d87618acaaecf7c09e26d
Cazeho <58745332+Cazeho@users.noreply.github.com>
2024-10-12 17:38:42 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# docker
+
+curl -s https://raw.githubusercontent.com/Cazeho/download_project/main/docker_ubuntu.sh | bash
+
+# learnhouse (course)
+cd /opt
+git clone https://github.com/learnhouse/learnhouse 
+cd learnhouse
+docker-compose up -d
+# https://docs.learnhouse.app/self-hosting/hosting-guide
